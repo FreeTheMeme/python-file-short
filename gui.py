f053ba309543649ee08682103a00325e8116a448
bigtin <you@example.com>
2023-08-06 17:13:08 -0400

    ported the factor for loop

--- gui.py
+++ gui.py
@@ -6,19 +6,31 @@ from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-
+#vars
+i = 1
+factor = 0
+answer = 0
 #the wigets
 ttk.Label(frm, text="v0.01").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 # user input 
 def input_factor():
-    factor_input = entry.get()
-    print("INPUT:", factor_input)
+    factor = int(entry.get())
+    for i in range (1, factor):
+        answer = factor / i
+    
+        if factor < factor/2:
+            continue
+        elif answer.is_integer():
+            ttk.Label
+    
+        else:
+            continue
 
 
 entry = ttk.Entry(frm)
-entry.grid(column=0,row=3)
-ttk.Button(frm, text="GO!", command=input_factor).grid(column=0,row=2)
+entry.grid(column=0,row=1)
+ttk.Button(frm, text="GO!", command=input_factor).grid(column=2,row=0)
 
 
 
