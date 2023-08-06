6b72965be179220c67701c7fc8718a03bb4a35ea
bigtin <you@example.com>
2023-08-06 14:19:17 -0400

    added user input

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-#bigtin 8/6/23 v0.01
+#bigtin 8/6/23 v0.02
 
 from tkinter import *
 from tkinter import ttk
@@ -10,10 +10,18 @@ frm.grid()
 #the wigets
 ttk.Label(frm, text="v0.01").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
-
 # user input 
-ttk.Entry().grid(column=0,row=3)
-ttk.Button(frm, text="GO!").grid(column=0,row=2)
+def input_factor():
+    factor_input = entry.get()
+    print("INPUT:", factor_input)
+
+
+entry = ttk.Entry(frm)
+entry.grid(column=0,row=3)
+ttk.Button(frm, text="GO!", command=input_factor).grid(column=0,row=2)
+
+
+
 
 #not sure what this but its very important :O
 root.mainloop()
