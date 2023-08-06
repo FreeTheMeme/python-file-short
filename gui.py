97b332e41b29cba7486286608dadd619b3708653
bigtin <you@example.com>
2023-08-06 13:44:20 -0400

    organize wigets added comments

--- gui.py
+++ gui.py
@@ -1,11 +1,19 @@
-#bigtin 8/1/23 v0
+#bigtin 8/6/23 v0.01
 
 from tkinter import *
 from tkinter import ttk
+#window stuff
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-ttk.Label(frm, text="Hello World!").grid(column=0, row=1)
+
+#the wigets
+ttk.Label(frm, text="v0.01").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+
+# user input 
+ttk.Entry().grid(column=0,row=3)
+ttk.Button(frm, text="GO!").grid(column=0,row=2)
+
+#not sure what this but its very important :|
 root.mainloop()
